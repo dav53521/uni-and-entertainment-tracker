a0abed2f5685b6b95213b133de06d5ab549aae6c
David Sorrell <56758485+dav53521@users.noreply.github.com>
2024-04-09 22:32:40 +1000

    Begun creating header

--- src/app/header/header.tsx
+++ src/app/header/header.tsx
@@ -1,2 +1,15 @@
-export default function header() {
+import React from "react"
+import Link from 'next/link'
+
+export default function SiteHeader() {
+    return (
+        <div className="flex">
+            <div className="text-2xl">
+                <Link href="/">Home</Link>
+            </div>
+            <div className="align">
+                <Link href="Assignments.tsx">Assignments</Link>
+            </div>
+        </div>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Link from "next/link";
+import SiteHeader from "./header/header"
+import RootLayout from "./layout";
 
 export default function Home() {
-  return (
-    <main>
-      <h1 className="text-6xl text-center">Hello</h1>
-    </main>
+  return(
+      <main>
+        <h1>Testing home page</h1>
+      </main>
   );
 }
