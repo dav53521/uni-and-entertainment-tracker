9642fbafdda857c699754e0f78dcaf35881e8a74
David Sorrell <56758485+dav53521@users.noreply.github.com>
2024-04-20 21:46:54 +1000

    Made test ensure that the header is correct

--- e2e/router.spec.ts
+++ e2e/router.spec.ts
@@ -3,9 +3,9 @@ import {test, expect} from '@playwright/test'
 test('Does home page exist', async ({ page }) => {
     await page.goto('http://localhost:3000/');
 
-    await expect(page.getByTestId("Home-Page-Title")).toBeVisible();
+    await expect(page.getByText("Welcome to My Uni and Entertainment Tracker")).toBeVisible();
 
     await page.getByTestId("Home-Link").click();
 
-    await expect(page.getByTestId("Home-Page-Title")).toBeVisible();
+    await expect(page.getByText("Welcome to My Uni and Entertainment Tracker")).toBeVisible();
 });
