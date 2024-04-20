f3efa18fe0258aa2d9d9603c3bc76ac0d14c1059
David Sorrell <56758485+dav53521@users.noreply.github.com>
2024-04-20 18:38:09 +1000

    Added playwright test to make sure that home page can be reached

---
+++ e2e/router.spec.ts
@@ -0,0 +1,7 @@
+import {test, expect} from '@playwright/test'
+
+test('Does home page exist', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+
+    await expect(page.getByTestId("Home-Page-Title")).toBeVisible();
+});
