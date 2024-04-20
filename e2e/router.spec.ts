962d57ee059b1b9841a9ef3c84636d3d278e93ce
David Sorrell <56758485+dav53521@users.noreply.github.com>
2024-04-20 18:39:58 +1000

    Updated test to test if react router home redirect was redirecting to the home page

--- e2e/router.spec.ts
+++ e2e/router.spec.ts
@@ -4,4 +4,8 @@ test('Does home page exist', async ({ page }) => {
     await page.goto('http://localhost:3000/');
 
     await expect(page.getByTestId("Home-Page-Title")).toBeVisible();
+
+    await page.getByTestId("Home-Link").click();
+
+    await expect(page.getByTestId("Home-Page-Title")).toBeVisible();
 });
