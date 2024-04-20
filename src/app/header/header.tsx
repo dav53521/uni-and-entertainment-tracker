f761e57407975e145bdeef9397d8175f03831d36
David Sorrell <56758485+dav53521@users.noreply.github.com>
2024-04-20 13:16:58 +1000

    Added more pages to the center

--- src/app/header/header.tsx
+++ src/app/header/header.tsx
@@ -3,12 +3,15 @@ import Link from 'next/link'
 
 export default function SiteHeader() {
     return (
-        <div className="flex">
-            <div className="text-2xl">
+        <div className="flex flex-row justify-center items-center">
+            <div className="align-middle text-2xl">
                 <Link href="/">Home</Link>
             </div>
-            <div className="align">
+            <div className="align-middle text-2xl relative left-3">
                 <Link href="Assignments.tsx">Assignments</Link>
+            </div>
+            <div className="align-middle text-2xl relative left-6 ">
+                <Link href="Subjects.tsx">Subjects</Link>
             </div>
         </div>
     );
