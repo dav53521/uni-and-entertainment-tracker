1e25af6c705f13a15f660926483c10984bd261d5
David Sorrell <david2017au@gmail.com>
2024-07-01 21:11:55 +1000

    Begun writting tests for db helper

--- tests/dbQueryHelper.test.ts
+++ tests/dbQueryHelper.test.ts
@@ -1,7 +1,7 @@
 import { describe, it } from "@jest/globals";
-import {dbConnectionAndQuery} from "../src/app/dbQueryHelper"
+import { executeQueryAsync } from "@/app/dbQueryHelper";
 
 describe("DB query helper connection", () => {
-    it("Can connect to correct db without throwing an exception", () => {
+    it("Can connect to db without throwing an exception when user details are correct", () => {
     })
 });
