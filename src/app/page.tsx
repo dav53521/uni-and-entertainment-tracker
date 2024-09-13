1367bc7e070b07298075161ea78de43b74787a36
David Sorrell <david2017au@gmail.com>
2024-09-13 23:44:37 +1000

    Removing uneeeded button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@mui/joy";
-
 export default function Home() {
   return(
       <main>
         <h1 className="text-3xl font-semibold flex flex-row justify-center">Welcome to My Uni and Entertainment Tracker</h1>
-        <Button color="primary">test</Button>
       </main>
   );
 }
